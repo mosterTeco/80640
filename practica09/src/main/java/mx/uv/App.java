7d2400e484bd70ef155ecf9060533dd5888d6b95
mosterTeco <reyesmls11@hotmail.com>
2023-10-19 12:55:07 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,4 +1,5 @@
 package mx.uv;
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -9,5 +10,20 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hola Mundo!" );
+        get("/", 
+            (request, response)-> "<h1>Hola Mundo!</h1>"
+        );
+        get("/r1", 
+            (request, response)-> "<h1>Adios Mundo!</h1>"
+        );
+        get("/r2", 
+            (request, response)-> "<h1>Que tal Mundo!</h1>"
+        );
+        get("/r3", 
+            (request, response)-> "<h1>Ok Mundo!</h1>"
+        );
+        get("/r4", 
+            (request, response)-> "{'alumno':'john','matricula':'s21','carerra':'tecnologias'}"
+        );
     }
 }
