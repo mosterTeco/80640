fd55984c6d961b54a6a49f5e7a8fd2c146d43fff
mosterTeco <reyesmls11@hotmail.com>
2023-11-09 15:25:30 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -9,6 +9,30 @@ public class App
 {
     public static void main( String[] args )
     {
+        options("/*",(request,response)->{
+
+        String accessControlRequestHeaders=request.headers("Access-Control-Request-Headers");
+
+        if(accessControlRequestHeaders!=null){
+
+        response.header("Access-Control-Allow-Headers",accessControlRequestHeaders);
+
+        }
+
+        String accessControlRequestMethod=request.headers("Access-Control-Request-Method");
+
+        if(accessControlRequestMethod!=null){
+
+        response.header("Access-Control-Allow-Methods",accessControlRequestMethod);
+
+        }
+
+        return "OK";
+
+        });
+
+        before((request,response)->response.header("Access-Control-Allow-Origin","*"));
+
         System.out.println( "Hola Mundo!" );
         get("/", 
             (request, response)-> "<h1>Hola Mundo!</h1>"
@@ -23,7 +47,7 @@ public class App
             (request, response)-> "<h1>Ok Mundo!</h1>"
         );
         get("/r4", 
-            (request, response)-> "{'alumno':'john','matricula':'s21','carerra':'tecnologias'}"
+            (request, response)-> "{\"alumno\":\"john\",\"matricula\":\"s21\",\"carerra\":\"tecnologias\"}"
         );
     }
 }
